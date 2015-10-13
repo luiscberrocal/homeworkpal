93b39fc732abccd356ae93e568b84d37c5739c68
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2015-10-12 20:35:33 -0500

    Added models to admin added sum data

--- homeworkpal_project/homework/admin.py
+++ homeworkpal_project/homework/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import School, SchoolLevel, Teacher, Student, Homework, Subject
+
+admin.site.register(School)
+admin.site.register(SchoolLevel)
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Homework)
+admin.site.register(Subject)
+
--- homeworkpal_project/homework/models.py
+++ homeworkpal_project/homework/models.py
@@ -1,4 +1,5 @@
 from autoslug import AutoSlugField
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -8,6 +9,26 @@ class School(models.Model):
 
     def __str__(self):
         return self.name
+
+class SchoolMember(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    middle_name = models.CharField(max_length=30, null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        if self.user.last_name and self.user.first_name:
+            return '%s, %s' % (self.user.last_name, self.user.first_name)
+        else:
+            return self.user.username
+
+
+class Teacher(SchoolMember):
+    pass
+
+class Student(SchoolMember):
+    pass
 
 
 class SchoolLevel(models.Model):
@@ -23,6 +44,7 @@ class Subject(models.Model):
     name = models.CharField(max_length=30)
     slug = AutoSlugField(populate_from='name', max_length=30)
     school_level = models.ForeignKey(SchoolLevel)
+    teacher = models.ForeignKey(Teacher)
 
     def __str__(self):
         return '%s (%s)' % (self.name, self.school_level.name)
@@ -39,4 +61,7 @@ class Homework(models.Model):
     due_date = models.DateField()
     description = models.TextField()
     evaluation = models.CharField(max_length=3,choices=HOMEWORK_TYPES)
+    teacher = models.ForeignKey(Teacher)
 
+    def __str__(self):
+        return '%s - %s' % (self.subject.name, self.description)
