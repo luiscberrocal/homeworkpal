4b70eca4b4ca798463b32379348e722481325fd8
LBerrocal <lberrocal@pancanal.com>
2015-10-14 12:19:03 -0500

    Added school_level to heading

--- homeworkpal_project/homework/views.py
+++ homeworkpal_project/homework/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import ListView
-from .models import Homework
+from .models import Homework, SchoolLevel
 
 
 class HomeworkListView(ListView):
     model = Homework
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeworkListView, self).get_context_data(**kwargs)
+        context['school_level'] = SchoolLevel.objects.get(slug=self.kwargs['school_level_slug'])
+        return context
 
     def get_queryset(self):
         query_set = super(HomeworkListView, self).get_queryset()
