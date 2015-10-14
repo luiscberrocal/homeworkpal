b2a13de99d400a7acbd6b2a59d51a4d11d71e181
LBerrocal <lberrocal@pancanal.com>
2015-10-14 11:57:04 -0500

    Added filter to list view

--- homeworkpal_project/homework/views.py
+++ homeworkpal_project/homework/views.py
@@ -7,3 +7,10 @@ from .models import Homework
 
 class HomeworkListView(ListView):
     model = Homework
+
+    def get_queryset(self):
+        query_set = super(HomeworkListView, self).get_queryset()
+        school_slug = self.kwargs['school_slug']
+        level_slug = self.kwargs['school_level_slug']
+        return query_set.filter(subject__school_level__school__slug__exact=school_slug,
+                                subject__school_level__slug__exact=level_slug)
