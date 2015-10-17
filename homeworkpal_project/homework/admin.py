8fadd9f66e03ae7fecf1e0f3b7c3922a1dd612a3
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2015-10-17 17:15:51 -0500

    Updated the Admin to display attributes

--- homeworkpal_project/homework/admin.py
+++ homeworkpal_project/homework/admin.py
@@ -3,10 +3,31 @@ from django.contrib import admin
 # Register your models here.
 from .models import School, SchoolLevel, Teacher, Student, Homework, Subject
 
+
+class HomeworkAdmin(admin.ModelAdmin):
+    list_display = ('id', 'due_date', 'school', 'subject', 'teacher', 'evaluation')
+    list_editable = ('due_date',)
+
+    def school(self, obj):
+        return obj.subject.school_level.school
+
+
+class SchoolLevelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'school', 'name')
+    list_editable = ('name',)
+
+
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'school', 'school_level', 'name')
+
+    def school(self, obj):
+        return obj.school_level.school
+
+
 admin.site.register(School)
-admin.site.register(SchoolLevel)
+admin.site.register(SchoolLevel, SchoolLevelAdmin)
 admin.site.register(Teacher)
 admin.site.register(Student)
-admin.site.register(Homework)
-admin.site.register(Subject)
+admin.site.register(Homework, HomeworkAdmin)
+admin.site.register(Subject, SubjectAdmin)
 
