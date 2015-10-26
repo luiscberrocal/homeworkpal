efbafa36c66954dd5bb71ea67098e8293d90d43b
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2015-10-25 21:40:04 -0500

    Export goals almost working

--- homeworkpal_project/project_admin/management/commands/export_goals.py
+++ homeworkpal_project/project_admin/management/commands/export_goals.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import os
 from django.core.management import BaseCommand
 from openpyxl import Workbook
-from openpyxl.styles import Alignment, Font, PatternFill
+from openpyxl.styles import Alignment, Font, PatternFill, Border, Side
 from employee.models import CompanyGroup
 from homeworkpal_project.settings.base import TEST_DATA_PATH
 from project_admin.models import ProjectGoal
@@ -11,6 +11,27 @@ __author__ = 'luiscberrocal'
 
 
 class Command(BaseCommand):
+
+    border = Border(left=Side(border_style='thin', color='FF000000'),
+                    right=Side(border_style='thin', color='FF000000'),
+                    top=Side(border_style='thin', color='FF000000'),
+                    bottom=Side(border_style='thin', color='FF000000'),
+                    diagonal=Side(border_style=None, color='FF000000'),
+                    diagonal_direction=0,
+                    outline=Side(border_style=None,
+                                 color='FF000000'),
+                    vertical=Side(border_style=None,
+                                  color='FF000000'),
+                    horizontal=Side(border_style=None,
+                                    color='FF000000')
+                    )
+
+    alignment=Alignment(horizontal='general',
+                            vertical='top',
+                            text_rotation=0,
+                            wrap_text=True,
+                            shrink_to_fit=False,
+                            indent=0)
 
     def add_arguments(self, parser):
         parser.add_argument('group')
@@ -33,36 +54,46 @@ class Command(BaseCommand):
                 col = 1
                 for title in headers:
                     cell = sheet.cell(column=col, row=3, value=title)
+                    cell.border = self.border
+                    cell.alignment = self.alignment
                     self._format_header(cell)
                     col += 1
                 row = 4
             ## No Proyecto A
             col = 1
-            sheet.cell(column=col, row=row, value=pos)
+            cell = sheet.cell(column=col, row=row, value=pos)
+            cell.border = self.border
+            cell.alignment = Alignment(horizontal='center', vertical='center')
             ## Proyecto B
             col += 1
             cell = sheet.cell(column=col, row=row, value=str(goal.project))
+            cell.border = self.border
             self._wrap_cell(cell)
             ## Description C
             col += 1
             cell = sheet.cell(column=col, row=row, value=str(goal.project.description))
             self._wrap_cell(cell)
+            cell.border = self.border
             ## Satifactory D
             col += 1
             statisfactory = '%.0f %% debe estar entregado para el %s' % (goal.expected_advancement*100,
                                                             goal.project.planned_end_date.strftime('%d-%m-%Y'))
             cell = sheet.cell(column=col, row=row, value=statisfactory)
             self._wrap_cell(cell)
+            cell.border = self.border
             ## Weight E
             col += 1
             cell = sheet.cell(column=col, row=row, value=goal.weight)
             cell.number_format = '0%'
             cell.alignment = Alignment(horizontal='general', vertical='top')
+            cell.border = self.border
             ## Deliverbales F
             col += 1
             dummy = 'Money\nCars\nCanal'
             cell = sheet.cell(column=col, row=row, value=dummy)
             self._wrap_cell(cell)
+            cell.border = self.border
+
             row += 1
 
             self._set_column_widths(sheet)
@@ -74,20 +105,14 @@ class Command(BaseCommand):
         self.stdout.write('Wrote %s'  % filename)
 
     def _wrap_cell(self, cell):
-        alignment=Alignment(horizontal='general',
-                            vertical='top',
-                            text_rotation=0,
-                            wrap_text=True,
-                            shrink_to_fit=False,
-                            indent=0)
-        cell.alignment = alignment
+        cell.alignment = self.alignment
 
     def _set_column_widths(self, sheet):
         sheet.column_dimensions["A"].width = 5.0
         sheet.column_dimensions["B"].width = 30.0
         sheet.column_dimensions["C"].width = 50.0
-        sheet.column_dimensions["D"].width = 5.0
-        sheet.column_dimensions["E"].width = 15.0
+        sheet.column_dimensions["D"].width = 20.0
+        sheet.column_dimensions["E"].width = 5.0
         sheet.column_dimensions["F"].width = 15.0
 
     def _title_font(self, cell):
