e281e6e035f9d53b740a2db46e1da7b988d5723c
Luis Carlos Berrocal <luis.berrocal.1942@gmail.com>
2015-10-29 19:01:00 -0500

    Added edit form basic configuration.

--- homeworkpal_project/project_admin/urls.py
+++ homeworkpal_project/project_admin/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
-from .views import ProjectListView, ProjectDetailView
+from .views import ProjectListView, ProjectDetailView, ProjectUpdateView
 
 __author__ = 'LBerrocal'
 
@@ -7,5 +7,6 @@ __author__ = 'LBerrocal'
 urlpatterns = patterns('',
                        url(r'^$', ProjectListView.as_view(), {'status': 'all'}),
                        url(r'^running/$', ProjectListView.as_view(), {'status': 'running'}),
-                       url(r'^(?P<pk>[\d]*)/$', ProjectDetailView.as_view(), name='project_detail')
+                       url(r'^(?P<pk>[\d]*)/$', ProjectDetailView.as_view(), name='project_detail'),
+                       url(r'^update/(?P<pk>[\d]*)/$', ProjectUpdateView.as_view(), name='project_update')
                        )
--- homeworkpal_project/project_admin/views.py
+++ homeworkpal_project/project_admin/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, UpdateView
 from .models import Project
 
 
@@ -19,3 +19,14 @@ class ProjectListView(ListView):
 class ProjectDetailView(DetailView):
     model = Project
     context_object_name = 'project'
+
+class ProjectUpdateView(UpdateView):
+    model = Project
+    fields = ['short_name', 'description', 'planned_start_date', 'planned_end_date', 'actual_start_date']
+
+    # actual_end_date = models.DateField(null=True, blank=True)
+    # slug = AutoSlugField(populate_from='short_name', max_length=60, unique=True)
+    # planned_man_hours = models.DecimalField(max_digits=7, decimal_places=2)
+    # type = models.CharField(max_length=8, choices=PROJECT_TYPES, default=MAIN_PROJECT)
+    # group = models.ForeignKey(CompanyGroup, null=True)
+    # priority = models.IntegerField(default=10, help_text='The lower the number the higher the priority')
