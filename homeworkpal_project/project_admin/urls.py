35d7efe70f44b0527b8ec878bcbe5c33f565b344
Luis Carlos Berrocal <luis.berrocal.1942@gmail.com>
2015-10-30 18:10:16 -0500

    Added delete and create view

--- homeworkpal_project/project_admin/urls.py
+++ homeworkpal_project/project_admin/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
-from .views import ProjectListView, ProjectDetailView, ProjectUpdateView
+from .views import ProjectListView, ProjectDetailView, ProjectUpdateView, ProjectCreateView, ProjectDeleteView
 
 __author__ = 'LBerrocal'
 
@@ -8,5 +8,7 @@ urlpatterns = patterns('',
                        url(r'^$', ProjectListView.as_view(),kwargs= {'status': 'all'}, name='all_projects'),
                        url(r'^running/$', ProjectListView.as_view(), kwargs={'status': 'running'}, name='running_projects'),
                        url(r'^(?P<pk>[\d]*)/$', ProjectDetailView.as_view(), name='project_detail'),
-                       url(r'^update/(?P<pk>[\d]*)/$', ProjectUpdateView.as_view(), name='project_update')
+                       url(r'^update/(?P<pk>[\d]*)/$', ProjectUpdateView.as_view(), name='project_update'),
+                       url(r'^create/$', ProjectCreateView.as_view(), name='project_create'),
+                        url(r'^delete/(?P<pk>[\d]*)/$', ProjectDeleteView.as_view(), name='project_delete'),
                        )
--- homeworkpal_project/project_admin/views.py
+++ homeworkpal_project/project_admin/views.py
@@ -1,7 +1,8 @@
+from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView, DetailView, UpdateView
+from django.views.generic import ListView, DetailView, UpdateView, CreateView, DeleteView
 from .models import Project
 
 
@@ -29,9 +30,13 @@ class ProjectUpdateView(UpdateView):
               'planned_end_date', 'actual_start_date', 'actual_end_date',
               'planned_man_hours', 'type', 'group', 'priority']
 
-    # actual_end_date = models.DateField(null=True, blank=True)
-    # slug = AutoSlugField(populate_from='short_name', max_length=60, unique=True)
-    # planned_man_hours = models.DecimalField(max_digits=7, decimal_places=2)
-    # type = models.CharField(max_length=8, choices=PROJECT_TYPES, default=MAIN_PROJECT)
-    # group = models.ForeignKey(CompanyGroup, null=True)
-    # priority = models.IntegerField(default=10, help_text='The lower the number the higher the priority')
+
+class ProjectCreateView(CreateView):
+    model = Project
+    fields = ['short_name', 'description', 'planned_start_date',
+              'planned_end_date', 'actual_start_date', 'actual_end_date',
+              'planned_man_hours', 'type', 'group', 'priority']
+
+class ProjectDeleteView(DeleteView):
+    model = Project
+    success_url = reverse_lazy('all_projects')
