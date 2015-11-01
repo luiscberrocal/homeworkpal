4fee3034b206487eb85f79214dd7199583d084b6
Luis Carlos Berrocal <luis.berrocal.1942@gmail.com>
2015-11-01 12:57:28 -0500

    Project members form inline added. Remove members or risk is not working.

--- homeworkpal_project/project_admin/mixins.py
+++ homeworkpal_project/project_admin/mixins.py
@@ -1,17 +1,19 @@
 __author__ = 'lberrocal'
 
 class AbstractProjectCreateUpdateMixin(object):
-    formset_class = None
+    formset_classes = None
 
     def get_context_data(self, **kwargs):
-        assert self.formset_class is not None, "No formset class specified"
+        assert self.formset_classes is not None, "No formset class specified"
         context = super().get_context_data(**kwargs)
         if self.request.POST:
-            context['line_formset'] = self.formset_class(
-                self.request.POST,
-                instance=self.object)
+            for formset_class in self.formset_classes:
+                context[formset_class[0]] = formset_class[1](
+                    self.request.POST,
+                    instance=self.object)
         else:
-            context['line_formset'] = self.formset_class(
+            for formset_class in self.formset_classes:
+                context[formset_class[0]] = formset_class[1](
                 instance=self.object)
         return context
 
@@ -24,12 +26,17 @@ class AbstractProjectCreateUpdateMixin(object):
 
     def form_valid(self, form):
         context = self.get_context_data()
-        line_formset = context['line_formset']
-        if not line_formset.is_valid():
-            return super().form_invalid(form)
-
-        self.object = form.save()
-        line_formset.instance = self.object
-        line_formset.save()
+        for formset_class in self.formset_classes:
+            line_formset = context[formset_class[0]]
+            if not line_formset.is_valid():
+                return super().form_invalid(form)
+        i = 1
+        for formset_class in self.formset_classes:
+            line_formset = context[formset_class[0]]
+            if i == 1:
+                self.object = form.save()
+            line_formset.instance = self.object
+            line_formset.save()
+            i += 1
 
         return super().form_valid(form)
