728a3a47e55df6aaacd73920424327e5fabf9d91
LBerrocal <lberrocal@pancanal.com>
2015-11-02 16:20:27 -0500

    Added employees proyects.

--- homeworkpal_project/employee/views.py
+++ homeworkpal_project/employee/views.py
@@ -2,7 +2,7 @@ from braces.views import LoginRequiredMixin
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Employee
 
 
@@ -12,3 +12,9 @@ class EmployeeListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         qs = Employee.objects.from_group(self.kwargs['group_slug'])
         return qs
+
+
+class EmployeeProjectsView(LoginRequiredMixin, DetailView):
+    model = Employee
+    context_object_name = 'employee'
+    template_name = 'employee/employee_projects.html'
