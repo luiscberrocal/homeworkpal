94f5624303450d7bdc8b3545f1b3e26db9e539c1
Luis Carlos Berrocal <luis.berrocal.1942@gmail.com>
2015-11-03 11:16:08 -0500

    Moved employee manager to managers file. Tested reading rest api

--- homeworkpal_project/employee/managers.py
+++ homeworkpal_project/employee/managers.py
@@ -4,6 +4,22 @@ from django.db.models import Manager
 __author__ = 'lberrocal'
 
 
+class EmployeeManager(Manager):
+
+    def get_queryset(self):
+        queryset = super(EmployeeManager, self).get_queryset().filter(user__is_active=True).select_related('user')
+        return queryset
+
+    def from_group(self, company_group):
+        from .models import CompanyGroupEmployeeAssignment
+        if isinstance(company_group, str):
+            group_assignemnts = CompanyGroupEmployeeAssignment.objects.filter(group__slug=company_group).select_related('employee', 'employee__user')
+        else:
+            group_assignemnts = CompanyGroupEmployeeAssignment.objects.filter(group=company_group).select_related('employee', 'employee__user')
+        employees_pk = list()
+        for group in group_assignemnts:
+            employees_pk.append(group.employee.pk)
+        return self.get_queryset().filter(pk__in=employees_pk).select_related('user')
 
 
 
