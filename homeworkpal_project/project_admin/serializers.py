cb20b15623290a9d46485e59ed8b852399e9ed0a
LBerrocal <lberrocal@pancanal.com>
2015-11-04 11:59:55 -0500

    Ability to add employee as member from employee project page

--- homeworkpal_project/project_admin/serializers.py
+++ homeworkpal_project/project_admin/serializers.py
@@ -5,16 +5,15 @@ from .models import ProjectMember, Project
 __author__ = 'lberrocal'
 
 
-class ProjectMemberSerializer(serializers.HyperlinkedModelSerializer):
+class ProjectMemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProjectMember
-        fields =  ('role', 'employee', 'project', 'start_date', 'end_date')
+        fields = ('role', 'employee', 'project', 'start_date', 'end_date')
 
 
-class ProjectSerializer(serializers.HyperlinkedModelSerializer):
+class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields =  ('short_name', 'description', 'planned_start_date', 'planned_end_date',
-                   'actual_start_date', 'actual_end_date', 'slug', 'planned_man_hours',
-                   'group', 'priority')
-
+        fields = ('pk', 'short_name', 'description', 'planned_start_date', 'planned_end_date',
+                  'actual_start_date', 'actual_end_date', 'slug', 'planned_man_hours',
+                  'group', 'priority')
