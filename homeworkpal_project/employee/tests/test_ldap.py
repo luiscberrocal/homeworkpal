a203c18e832f3ba058cee30f8d41a7423bda6164
LBerrocal <lberrocal@pancanal.com>
2016-01-04 13:56:34 -0500

    LDAP test

--- homeworkpal_project/employee/tests/test_ldap.py
+++ homeworkpal_project/employee/tests/test_ldap.py
@@ -5,6 +5,6 @@ from django_auth_ldap.backend import LDAPBackend
 class TestLDAP(TestCase):
 
     def test_get_user(self):
-        user = LDAPBackend().populate_user('LBerrocal')
+        user = LDAPBackend().populate_user('OAHerrera')
         self.assertIsNotNone(user)
         self.assertEqual('Berrocal Cordoba', user.last_name)
