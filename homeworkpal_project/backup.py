64ec9dc1ffdad9d56b27df25e07843d2685be26c
LBerrocal <lberrocal@pancanal.com>
2016-02-05 17:03:30 -0500

    Added dropbox backup support.

--- homeworkpal_project/backup.py
+++ homeworkpal_project/backup.py
@@ -2,13 +2,19 @@ import datetime
 import os
 import shutil
 
+import sys
+from dropbox import dropbox
+from dropbox.exceptions import ApiError
+from dropbox.files import WriteMode
+
+from homeworkpal_project.settings.local_acp import DROPBOX_TOKEN
+
 __author__ = 'LBerrocal'
 
 
-def filename_with_datetime(file_path, base_filename):
+def filename_with_datetime(base_filename):
     '''
     os.path.join(TEST_OUTPUT_PATH, '%s_%s.xlsx' % ('maximo_tickets', timezone.now().strftime('%Y%m%d_%H%M')))
-    :param file_path:
     :param base_filename:
     :return:
     '''
@@ -16,14 +22,36 @@ def filename_with_datetime(file_path, base_filename):
     if len(parts) > 2:
         raise ValueError('Base filename cannot contain more the one dot')
     str_date = datetime.datetime.now().strftime('%Y%m%d_%H%M')
-    return os.path.join(file_path, '%s_%s.%s' % (parts[0], str_date, parts[1]))
+    return '%s_%s.%s' % (parts[0], str_date, parts[1])
+
+
+def upload_to_dropbox(source, target_filename):
+    dbx = dropbox.Dropbox(DROPBOX_TOKEN)
+    with open(source, 'rb') as f:
+        try:
+            dbx.files_upload(f, target_filename, mode=WriteMode('overwrite'))
+        except ApiError as err:
+            # This checks for the specific error where a user doesn't have
+            # enough Dropbox space quota to upload this file
+            if (err.error.is_path() and
+                    err.error.get_path().error.is_insufficient_space()):
+                sys.exit("ERROR: Cannot back up; insufficient space.")
+            elif err.user_message_text:
+                print(err.user_message_text)
+                sys.exit()
+            else:
+                print(err)
+                sys.exit()
 
 if __name__ == '__main__':
     source = r'C:\Users\lberrocal\PycharmProjects\homeworkpal\homeworkpal_project\homeworkpal_project\homework_pal.sqlite3'
     _, base_filename = os.path.split(source)
     backup_paths = [r'F:\backup_homeworkpal', r'C:\Users\lberrocal\OneDrive - Autoridad del Canal de Panama\2016_Project_admin']
+    dated_filename = filename_with_datetime(base_filename)
     for b_path in backup_paths:
         if os.path.exists(b_path):
-            target_filename = filename_with_datetime(b_path, base_filename)
+            target_filename = os.path.join(b_path, dated_filename)
             shutil.copy(source, target_filename)
             print('Backup to %s' % target_filename)
+    upload_to_dropbox(source, '/%s' % dated_filename)
+    print('Backup to dropbox %s' % dated_filename)
