f68b809b949e50013c608caab4a04995f4229ab8
LBerrocal <lberrocal@pancanal.com>
2016-03-07 15:36:19 -0500

    Cleaning of imports in extract_commits

--- homeworkpal_project/jira_git/management/commands/extract_commits.py
+++ homeworkpal_project/jira_git/management/commands/extract_commits.py
@@ -4,15 +4,13 @@ from django.core.management import BaseCommand
 
 from common.utils import filename_with_datetime
 from homeworkpal_project.settings.base import TEST_OUTPUT_PATH
-from jira_git.git_utils import create_git_excel
+from ...git_utils import create_git_excel
 
 __author__ = 'lberrocal'
 
 
 class Command(BaseCommand):
-    '''
-    This command will extract into an excel file all commits of git respositories found on a folder
-    '''
+
     def add_arguments(self, parser):
         parser.add_argument('folder')
         parser.add_argument("-o", "--output",
@@ -25,4 +23,3 @@ class Command(BaseCommand):
         output_filename = filename_with_datetime(TEST_OUTPUT_PATH, '%s_commits.xlsx' % last_folder)
         create_git_excel(options['folder'], output_filename)
         self.stdout.write('Wrote %s' % output_filename)
-        
